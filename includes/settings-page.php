d6cfc83b68e1281d3c38f214894c9ff6fa95e26a
Mukesh Patel <24436767+TheMukeshPatel@users.noreply.github.com>
2023-12-12 13:48:18 +0530

    Update settings-page.php

--- includes/settings-page.php
+++ includes/settings-page.php
@@ -11,14 +11,17 @@ require_once(plugin_dir_path(__FILE__) . 'encryption-functions.php');
 // Create custom plugin settings menu
 add_action('admin_menu', 'smtp_connector_for_wp_create_menu');
 
-function smtp_connector_for_wp_create_menu() {
+function smtp_connector_for_wp_create_menu()
+{
     add_options_page('SMTP Connector Settings', 'SMTP Connector', 'manage_options', 'smtp-connector-for-wp', 'smtp_connector_for_wp_settings_page');
     add_action('admin_init', 'smtp_connector_for_wp_settings_register');
 }
 
-function smtp_connector_for_wp_settings_register() {
+function smtp_connector_for_wp_settings_register()
+{
     // Sanitization function
-    function smtp_connector_for_wp_sanitize_option($input) {
+    function smtp_connector_for_wp_sanitize_option($input)
+    {
         return sanitize_text_field($input);
     }
 
@@ -33,18 +36,21 @@ function smtp_connector_for_wp_settings_register() {
     register_setting('smtp-connector-for-wp-settings-group', 'smtp_connector_for_wp_port', 'smtp_connector_for_wp_sanitize_option');
 }
 
-function smtp_connector_for_wp_encrypt_password($password) {
+function smtp_connector_for_wp_encrypt_password($password)
+{
     return encrypt_password($password);
 }
 
-function smtp_connector_for_wp_settings_page() {
+function smtp_connector_for_wp_settings_page()
+{
     if (!current_user_can('manage_options')) {
         wp_die(__('You do not have sufficient permissions to access this page.'));
     }
     ?>
     <div class="wrap">
         <h2>SMTP Connector Settings</h2>
-        <p>Enter your SMTP details below. Check <a target="_blank" href="https://mpateldigital.com/smtp-connector/">this tutorial</a> to know more about configuration</p>
+        <p>Enter your SMTP details below. Check <a target="_blank" href="https://mpateldigital.com/smtp-connector/">this
+                tutorial</a> to know more about configuration</p>
         <div id="poststuff">
             <div id="post-body" class="metabox-holder columns-2">
                 <!-- Main Content Area -->
@@ -56,22 +62,35 @@ function smtp_connector_for_wp_settings_page() {
                         <table class="form-table">
                             <tr valign="top">
                                 <th scope="row">SMTP Host</th>
-                                <td><input type="text" name="smtp_connector_for_wp_host" value="<?php echo esc_attr(get_option('smtp_connector_for_wp_host')); ?>" /></td>
+                                <td><input type="text" name="smtp_connector_for_wp_host"
+                                        value="<?php echo esc_attr(get_option('smtp_connector_for_wp_host')); ?>" /></td>
                             </tr>
 
                             <tr valign="top">
                                 <th scope="row">From Name</th>
-                                <td><input type="text" placeholder="Enter Website Name" name="smtp_connector_for_wp_from_name" value="<?php echo esc_attr(get_option('smtp_connector_for_wp_from_name')); ?>" /></td>
+                                <td><input type="text" placeholder="Enter Website Name"
+                                        name="smtp_connector_for_wp_from_name"
+                                        value="<?php echo esc_attr(get_option('smtp_connector_for_wp_from_name')); ?>" />
+                                </td>
                             </tr>
 
                             <tr valign="top">
                                 <th scope="row">SMTP Username</th>
-                                <td><input type="text" name="smtp_connector_for_wp_username" value="<?php echo esc_attr(get_option('smtp_connector_for_wp_username')); ?>" /></td>
+                                <td><input type="text" name="smtp_connector_for_wp_username"
+                                        value="<?php echo esc_attr(get_option('smtp_connector_for_wp_username')); ?>" />
+                                </td>
                             </tr>
 
                             <tr valign="top">
                                 <th scope="row">SMTP Password</th>
-                                <td><input type="password" name="smtp_connector_for_wp_password" value="<?php echo decrypt_password(get_option('smtp_connector_for_wp_password')); ?>" /></td>
+                                <td><input type="password" name="smtp_connector_for_wp_password"
+                                        value="<?php echo decrypt_password(get_option('smtp_connector_for_wp_password')); ?>" />
+                                    <span>Use App Password for <a target="_blank"
+                                            href="https://myaccount.google.com/apppasswords">Gmail</a> and <a
+                                            target="_blank"
+                                            href="https://help.zoho.com/portal/en/kb/bigin/channels/email/articles/generate-an-app-specific-password#To_generate_app_specific_password_for_Zoho_Mail">Zoho</a>.</span>
+                                </td>
+
                             </tr>
 
                             <tr valign="top">
@@ -87,7 +106,9 @@ function smtp_connector_for_wp_settings_page() {
 
                             <tr valign="top">
                                 <th scope="row">SMTP Port</th>
-                                <td><input type="number" placeholder="587 Most in Cases like Gmail" name="smtp_connector_for_wp_port" value="<?php echo esc_attr(get_option('smtp_connector_for_wp_port')); ?>" />
+                                <td><input type="number" placeholder="587 Most in Cases like Gmail"
+                                        name="smtp_connector_for_wp_port"
+                                        value="<?php echo esc_attr(get_option('smtp_connector_for_wp_port')); ?>" />
                                     <span>Use 587 most in cases. Gmail, Zoho, Sandinblue, Sandgrid etc use this port.</span>
                                 </td>
                             </tr>
