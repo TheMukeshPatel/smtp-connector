26f2b07172458ea1a2b84d59b08942221064082a
Mukesh Patel <24436767+TheMukeshPatel@users.noreply.github.com>
2023-12-12 13:20:44 +0530

    Add files via upload

---
+++ includes/encryption-functions.php
@@ -0,0 +1,23 @@
+<?php
+
+// Include this file where encryption and decryption functions are needed
+
+define('SMTP_SECURITY_KEY', SECURE_AUTH_KEY); // Actual constant security key
+
+function encrypt_password($password) {
+    $cipher = 'aes-256-cbc';
+    $ivlen = openssl_cipher_iv_length($cipher);
+    $iv = openssl_random_pseudo_bytes($ivlen);
+    $encrypted = openssl_encrypt($password, $cipher, SMTP_SECURITY_KEY, 0, $iv);
+    return base64_encode($iv . $encrypted);
+}
+
+function decrypt_password($encrypted_password) {
+    $cipher = 'aes-256-cbc';
+    $data = base64_decode($encrypted_password);
+    $ivlen = openssl_cipher_iv_length($cipher);
+    $iv = substr($data, 0, $ivlen);
+    $decrypted = openssl_decrypt(substr($data, $ivlen), $cipher, SMTP_SECURITY_KEY, 0, $iv);
+    return $decrypted;
+}
+?>
