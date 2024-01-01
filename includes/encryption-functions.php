b05e81b077ec10f3a2d984744147c9c031a45475
Mukesh Patel <mukeshpatel.rajasthan@gmail.com>
2024-01-02 00:08:21 +0530

    Fixed decrpte_password issue
    Fixed issue while decrypting password due to some wrong function name

--- includes/encryption-functions.php
+++ includes/encryption-functions.php
@@ -18,10 +18,10 @@ function smtp_connector_for_wp_encrypted_password($password)
     return base64_encode($iv . $encrypted);
 }
 
-function smtp_connector_for_wp_decrypted_password($encrypted_password)
+function smtp_connector_for_wp_decrypt_password($password)
 {
     $cipher = 'aes-256-cbc';
-    $data = base64_decode($encrypted_password);
+    $data = base64_decode($password);
     $ivlen = openssl_cipher_iv_length($cipher);
     $iv = substr($data, 0, $ivlen);
     $decrypted = openssl_decrypt(substr($data, $ivlen), $cipher, SMTP_SECURITY_KEY, 0, $iv);
